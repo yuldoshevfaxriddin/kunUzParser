afbfd5eb0e298cd2dc1c85f9150caf05bca48d46
yuldoshevfaxriddin <faxriddinyuldoshev56@gmail.com>
2024-04-06 22:18:43 +0500

    parser version=2.0.0 start

---
+++ parser_kun_uz_v2.py
@@ -0,0 +1,23 @@
+import requests
+import bs4
+
+def getCategory(base_html):
+    # returned list categorys
+    pass
+
+def getRegion(base_html):
+    # returned list regions
+    pass
+
+def getHtml(base_url):
+    # returned html
+    pass
+
+def getBs4(base_html):
+    # returned bs4 object
+    pass
+
+
+if __name__=='__main__':
+    # running code
+    pass
